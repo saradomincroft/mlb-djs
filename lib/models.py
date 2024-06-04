d67fb1899dc580b566eabd2a01d344ac9f53848a
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-06-05 00:03:54 +1000

    updated Dj class, added Genre, DjGenre, Subgenre, DjSubgenre, Venue, DjVenue

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, String, Integer, Boolean, ForeginKey
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.ext.associationproxy import association_proxy
 
 Base = declarative_base()
 
@@ -7,7 +8,93 @@ class Dj(Base):
     __tablename__ = "djs"
 
     id = Column(Integer(), primary_key=True)
-    djname = Column(String(), nullable=False)
-    genre = Column(String(), nullable=False)
-    subgenre = Column(String(), nullable=False)
+    name = Column(String(), nullable=False)
     produces = Column(Boolean(), nullable=False)
+
+    dj_genres = relationship("DjGenre", back_populates="dj")
+    genres = association_proxy("dj_genres", "genre", creator=lambda g: DjGenre(genre=g))
+
+    dj_subgenres = relationship("DjSubgenre", back_populates="dj")
+    subgenres = association_proxy("dj_subgenres", "subgenre", creator=lambda s: DjSubgenre(subgenre=s))
+
+    dj_venues = relationship("DjVenue", back_populates="dj")
+    venues = association_proxy("dj_venues", "venue", creator=lambda v: DjVenue(venue=v))
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False, unique=True)
+
+    subgenres = relationship("Subgenre", back_populates="genre")
+
+    dj_genres = relationship("DjGenre", back_populates="genre")
+    djs = association_proxy("dj_genres", "dj", creator=lambda dg: DjGenre(dj=dg))
+
+    def __repr__(self):
+        return f"{self.title}"
+
+class DjGenre(Base):
+    __tablename__ = "dj_genres"
+
+    id = Column(Integer, primary_key=True)
+    dj_id = Column(Integer, ForeignKey('djs.id'))
+    genre_id = Column(Integer, ForeignKey('genres.id'))
+
+    dj = relationship('Dj', back_populates='dj_genres')
+    genre = relationship('Genre', back_populates='dj_genres')
+
+
+class Subgenre(Base):
+    __tablename__ = "subgenres"
+
+    id = Column(Integer, primary_key=True)
+    subtitle = Column(String, nullable=False)
+    genre_id = Column(Integer, ForeignKey("genres.id"))
+
+    genre = relationship("Genre", back_populates="subgenres")
+
+    dj_subgenres = relationship("DjSubgenre", back_populates="subgenre")
+    djs = association_proxy("dj_subgenres", "dj", creator=lambda ds: DjSubgenre(dj=ds))
+
+    def __repr__(self):
+        return f"{self.subtitle}"
+
+
+class DjSubgenre(Base):
+    __tablename__ = "dj_subgenres"
+
+    id = Column(Integer, primary_key=True)
+    dj_id = Column(Integer, ForeignKey('djs.id'))
+    subgenre_id = Column(Integer, ForeignKey('subgenres.id'))
+
+    dj = relationship('Dj', back_populates='dj_subgenres')
+    subgenre = relationship('Subgenre', back_populates='dj_subgenres')
+
+
+class Venue(Base):
+    __tablename__ = "venues"
+
+    id = Column(Integer, primary_key=True)
+    venuename = Column(String, nullable=False)
+
+    dj_venues = relationship("DjVenue", back_populates="venue")
+    djs = association_proxy("dj_venues", "dj", creator=lambda dv: DjVenue(dj=dv))
+
+    def __repr__(self):
+        return f"{self.venuename}"
+    
+
+class DjVenue(Base):
+    __tablename__ = "dj_venues"
+
+    id = Column(Integer, primary_key=True)
+    dj_id = Column(Integer, ForeignKey('djs.id'))
+    venue_id = Column(Integer, ForeignKey('venues.id'))
+
+    dj = relationship('Dj', back_populates='dj_venues')
+    venue = relationship('Venue', back_populates='dj_venues')
